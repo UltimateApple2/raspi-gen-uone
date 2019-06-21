a00baf4d5cb50fb997ae7c44871c443a716bcf8a
philtimmes <philtimmes@gmail.com>
2019-06-21 16:53:04 -0400

    new file:   README.md 	modified:   stage3/00-install-packages/00-packages 	modified:   stage3/00-install-packages/01-run.sh

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -5,4 +5,8 @@ update-alternatives --install /usr/bin/x-www-browser \
   x-www-browser /usr/bin/chromium-browser 86
 update-alternatives --install /usr/bin/gnome-www-browser \
   gnome-www-browser /usr/bin/chromium-browser 86
+cd /opt
+git clone --depth 1 https://github.com/arduino/Arduino.git arduino
+cd arduino/build
+yes "" | ant dist
 EOF
