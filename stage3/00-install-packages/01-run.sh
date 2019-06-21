3ae2c3f104cee2c26457fc59fa2ea0c476ad9c9c
philtimmes <philtimmes@gmail.com>
2019-06-21 17:35:18 -0400

    Fix CWD
    Move to correct directory after building Arduino IDE

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -5,6 +5,7 @@ update-alternatives --install /usr/bin/x-www-browser \
   x-www-browser /usr/bin/chromium-browser 86
 update-alternatives --install /usr/bin/gnome-www-browser \
   gnome-www-browser /usr/bin/chromium-browser 86
+CURRDUR=$CWD
 cd /opt
 echo "Cloning Current Arduino Build"
 wget https://github.com/arduino/Arduino/archive/master.zip -O arduino-master.zip
@@ -13,3 +14,4 @@ echo "Cloned..."
 cd Arduino-master/build
 echo "Changed directory..."
 yes "" | ant dist
+cd $CURRDIR
