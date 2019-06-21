50a126c539fa7a79e56329f483f53399f850cc1c
philtimmes <philtimmes@gmail.com>
2019-06-21 17:18:12 -0400

    Notes in build of arduino IDE

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -6,7 +6,9 @@ update-alternatives --install /usr/bin/x-www-browser \
 update-alternatives --install /usr/bin/gnome-www-browser \
   gnome-www-browser /usr/bin/chromium-browser 86
 cd /opt
+echo "Cloning Current Arduino Build"
 git clone --depth 1 https://github.com/arduino/Arduino.git arduino
+echo "Cloned..."
 cd arduino/build
+echo "Changed directory..."
 yes "" | ant dist
-EOF
